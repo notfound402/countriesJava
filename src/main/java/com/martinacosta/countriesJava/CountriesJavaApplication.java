650c2a4c441f19acfd13aaec7c432d338dd5ee75
notfound402 <martinacosta1234@gmail.com>
2023-09-18 19:31:19 -0300

    countries api base

---
+++ src/main/java/com/martinacosta/countriesJava/CountriesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.martinacosta.countriesJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesJavaApplication.class, args);
+	}
+
+}
